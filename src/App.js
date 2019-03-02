a74bb7e032e676344d991213b30ca90539c5e716
Pa Yeng Thao <payengt24@gmail.com>
2019-03-01 21:23:54 -0600

    added Spinner while page/data is being load

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import './SeasonDisplay'
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'
 
 
 
@@ -36,7 +37,7 @@ class App extends Component {
       return <SeasonDisplay latitude={this.state.latitude}/>
     }
 
-    return <div>Loading</div>
+    return <Spinner />
 
   }
 }
